8732eee44bb5548f8877ef184c16d341cccd774a
Shatilla Prayer <shatilla.prayer@unlv.edu>
2016-07-05 12:27:03 -0700

    functional audio

--- timeline/js/timelineCustom.js
+++ timeline/js/timelineCustom.js
@@ -38,25 +38,30 @@ $(document).bind('keyup', function(e) {
     if (key == 39 && disableKey == false) {
         //right arrow key - go to next slide
         timeline.goToNext(); // go to next slide
+        sound = false;
+        audioSound();
 
     } else if (key == 37 && disableKey == false) {
         //left arrow key - go to previous slide
         timeline.goToPrev(); // go to previous slide
-
-
+        sound = false;
+        audioSound();
     } else if (key == 82) {
         location.reload(); //reload app - r key
     } else if (key == 83) {
         intro(); //Start App - s key
-        $('.sound').html(soundOff); // init icon 
+       // $('.sound').html(soundOff); // init icon 
     } else if (key == 65 && disableKey == false) {
         // A key - toggle Sound
         // toggles sound and changes icon based on whether sound is on or off
+        sound = !sound;
+        sound ? playSound(getSlideIndex()) : stopSound(getSlideIndex());
         changeSoundIcon();
     } else if (key == 72 && disableKey == false) {
         // H key - Help Menu
         // toggles sound and changes icon based on whether sound is on or off
         openHelp = !openHelp;
+        sound = false;
         if (openHelp == true) {
             showHelpMenu();
             $('.help_Button').html("Close <i class='fa fa-times'></i>");
@@ -69,7 +74,8 @@ $(document).bind('keyup', function(e) {
 // start sound on intro
 $('.help_Button').click(function() {
     intro(); //Start App - s key
-    $('.sound').html(soundOn); // init icon 
+    sound = false;
+    $('.sound').html(soundOff); // init icon 
 });
 
 // reload app 
@@ -92,34 +98,30 @@ $('.help_Button').click(function() {
 $('.sound').click(function() {
     // toggles sound and changes icon based on whether sound is on or off
     changeSoundIcon();
+    sound = !sound;
+    sound ? playSound(getSlideIndex()) : stopSound(getSlideIndex());
+    changeSoundIcon();
 });
-
-// grayscale
-/*$('.grayscale').click(function(){
-    var buttonColor = tinycolor("rgb(234,0,31)").desaturate(100).toString();
-    $('.btn').css({
-        'background-color': buttonColor
-    });
-
-    $('.btn').css({
-        'background-color': buttonColor
-    });
-    console.log('grayscale', buttonColor);
-});*/
 
 // read when user clicks next
 timeline.on("nav_next", function(data) {
-
+    sound = false;
+    audioSound();
+    changeSoundIcon();
 });
 
 // read when user clicks previous
 timeline.on("nav_previous", function(data) {
-
+    sound = false;
+    audioSound();
+    changeSoundIcon();
 });
 
 // read on slide change
 timeline.on("change", function(data) {
-
+    sound = false;
+    audioSound();
+    changeSoundIcon();
 });
 
 function intro() {
@@ -154,22 +156,26 @@ function stopSound(current) {
     }
 }
 
+function audioSound() {
+    if (sound == false) {
+        stopSound(getSlideIndex());
+        $('.sound').html(soundOff);
+    } else {
+        playSound(getSlideIndex());
+        ('.sound').html(soundOn)
+    }
+}
 
-function toggleSound() {
+/*function toggleSound() {
     // turn sound on/off with keystroke
     // check if sound is playing and turn on
     sound = !sound; //toggle sound state
-    if (sound == false) {     
-        stopSound(getSlideIndex());
-    } else {
-        playSound(getSlideIndex());
-    }
-    // console.log('toggleSound()', sound);
     return sound;
-}
+}*/
 
 function changeSoundIcon() {
-    toggleSound() ? $('.sound').html(soundOn) : $('.sound').html(soundOff);
+  //  sound = !sound;
+    sound ? $('.sound').html(soundOn) : $('.sound').html(soundOff);
 }
 
 
